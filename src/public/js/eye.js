2f15e4d7ed58cf7c18afe85e1a23568e9484804d
choistar <cys9813@naver.com>
2022-12-09 00:09:40 +0900

    2차 최종

--- src/public/js/eye.js
+++ src/public/js/eye.js
@@ -2,11 +2,7 @@
 
 
 
-
-
-
-
-const DrawIrisResult = (left, right) => {
+const DrawEyeResult = (left, right) => {
     const left_eye_list = left;
     const right_eye_list = right;
     console.log(left_eye_list);
@@ -17,60 +13,61 @@ const DrawIrisResult = (left, right) => {
     var rightctx = c2.getContext("2d");
 
     // 가로 세로 선 그리기
-    leftctx.strokeStyle = "#000000";
-    leftctx.lineWidth = 1;
+    leftctx.strokeStyle = rightctx.strokeStyle ="#000000";
+    leftctx.lineWidth = rightctx.lineWidth = 1;
     // 세로 축
     leftctx.beginPath();
     leftctx.moveTo(0, 250);
     leftctx.lineTo(500, 250);
     leftctx.stroke();
+
+    rightctx.beginPath();
+    rightctx.moveTo(0, 250);
+    rightctx.lineTo(500, 250);
+    rightctx.stroke();
+
     // 가로 축
     leftctx.beginPath();
     leftctx.moveTo(250, 0);
     leftctx.lineTo(250, 500);
     leftctx.stroke();
 
-    // 가로 세로 선 그리기
-    rightctx.strokeStyle = "#000000";
-    rightctx.lineWidth = 1;
-    // 세로 축
-    rightctx.beginPath();
-    rightctx.moveTo(0, 250);
-    rightctx.lineTo(500, 250);
-    rightctx.stroke();
-    // 가로 축
+    
     rightctx.beginPath();
     rightctx.moveTo(250, 0);
     rightctx.lineTo(250, 500);
     rightctx.stroke();
 
+    //눈동자 좌표찍기위한 선 색 지정 바꾸기 가능
     leftctx.fillStyle = "#000000";
     leftctx.strokeStyle = "000000";
     rightctx.fillStyle = "000000";
     rightctx.strokeStyle = "000000";
-    
-    for (let i = 1; i < left_eye_list.length; i++) {
-      // console.log(left_eye_list[i].x);
-      leftctx.beginPath();
-      leftctx.arc(
-        left_eye_list[i].x - left_eye_list[0].x + 250,
-        left_eye_list[i].y - left_eye_list[0].y + 250,
-        5,
-        0,
-        2 * Math.PI
-      );
 
+    let left_Xsum=left_Ysum=right_Xsum=right_Ysum = 0;
+    const array_len = left_eye_list.length;
+    console.log(array_len);
+    for (let i = 1; i < array_len; i++) {
+      left_Xsum += left_eye_list[i].x;
+      left_Ysum += left_eye_list[i].y;
+      right_Xsum += right_eye_list[i].x;
+      right_Ysum += right_eye_list[i].y;
+    }
+
+    const left_avg = {x:left_Xsum/array_len, y: left_Ysum/array_len};
+    const right_avg = {x:right_Xsum/array_len, y: right_Ysum/array_len};
+
+    
+    for (let i = 1; i < array_len; i++) {
+      leftctx.beginPath();
+      leftctx.arc(left_eye_list[i].x - left_avg.x + 250,left_eye_list[i].y - left_avg.y + 250,5,0,2 * Math.PI);
       leftctx.stroke();
       leftctx.fill();
+    }
 
+    for (let i = 1; i < array_len; i++) {
       rightctx.beginPath();
-      rightctx.arc(
-        right_eye_list[i].x - right_eye_list[0].x + 250,
-        right_eye_list[i].y - right_eye_list[0].y + 250,
-        5,
-        0,
-        2 * Math.PI
-      );
+      rightctx.arc(right_eye_list[i].x - right_avg.x + 250,right_eye_list[i].y - right_avg.y + 250,5,0,2 * Math.PI);
       rightctx.stroke();
       rightctx.fill();
     }
@@ -82,7 +79,7 @@ const DrawIrisResult = (left, right) => {
         console.log(data);
         const left_eye_list = data.left;
         const right_eye_list = data.right;
-        DrawIrisResult(left_eye_list,right_eye_list);
+        DrawEyeResult(left_eye_list,right_eye_list);
     });
 
 
