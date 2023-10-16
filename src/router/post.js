81730139caf91e4ba24cfd26953cdbd74fd691c6
김형태 <rlarnsaas123@naver.com>
2023-10-16 17:24:46 +0900

    10/16

--- src/router/post.js
+++ src/router/post.js
@@ -84,6 +84,17 @@ router.get("/", (req, res) => {
         }
     })
 })
+//제목 검색 API
+.post("/search", (req, res) => {
+    pool.query("insert into post set ?", req.body, function(err, result) {
+        if(err) {
+            console.log(err);
+            res.send({data: "err"})
+        } else{
+            res.send({data: "ok"})
+        }
+    })
+})
 .patch("/update/:id", (req, res) => {
     pool.query("update post set ? where id = ?", [req.body, req.params.id], function(err, result) {
         if(err) {
